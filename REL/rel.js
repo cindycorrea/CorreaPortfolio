b9f4c8e04d6a9de4fe21b8c5d1ee2e36f055bd58
cindycorrea <cindycorrea04@gmailc.om>
2022-03-18 09:03:58 -0600

    updated menu

---
+++ REL/rel.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    var updateElement = document.getElementById("menu");
+ updateElement.classList.toggle("open");
+
+ var updateElement = document.getElementById("menu-content");
+ updateElement.classList.toggle("menu-open");
+}       
